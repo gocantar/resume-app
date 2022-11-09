782f7c09db86b74f850cf7957b2027d0dd6da52c
Gonzalo Cantarero <gocantar@gmail.com>
2022-11-09 17:23:46 +0100

    style: update app colors

--- app/src/main/java/com/gocantar/resume/ui/theme/Color.kt
+++ app/src/main/java/com/gocantar/resume/ui/theme/Color.kt
@@ -2,7 +2,9 @@ package com.gocantar.resume.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple200 = Color(0xFFBB86FC)
-val Purple500 = Color(0xFF6200EE)
-val Purple700 = Color(0xFF3700B3)
-val Teal200 = Color(0xFF03DAC5)
+val Purple400 = Color(0xFFAB47BC)
+val Purple700 = Color(0xFF7B1FA2)
+val Purple800 = Color(0xFF6A1B9A)
+
+val Teal700 = Color(0xFF00796B)
+val Teal800 = Color(0xFF00695C)
--- app/src/main/java/com/gocantar/resume/ui/theme/Theme.kt
+++ app/src/main/java/com/gocantar/resume/ui/theme/Theme.kt
@@ -7,16 +7,17 @@ import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
 
 private val DarkColorPalette = darkColors(
-    primary = Purple200,
-    primaryVariant = Purple700,
-    secondary = Teal200
+    primary = Purple700,
+    primaryVariant = Purple800,
+    secondary = Teal700,
+    secondaryVariant = Teal800
 )
 
 private val LightColorPalette = lightColors(
-    primary = Purple500,
+    primary = Purple400,
     primaryVariant = Purple700,
-    secondary = Teal200
-
+    secondary = Teal700,
+    secondaryVariant = Teal800
     /* Other default colors to override
     background = Color.White,
     surface = Color.White,
